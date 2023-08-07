f765c72f0292f8776b8781453297a311b72fd533
Camila Paladines <camilapaladines27@gmail.com>
2023-08-07 17:07:55 -0500

    Add Card component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
+import Card from '../../components/Card/Card';
 
 export default function Home() {
-	return <Layout>Home</Layout>;
+	return (
+		<Layout>
+			<Card />
+		</Layout>
+	);
 }
