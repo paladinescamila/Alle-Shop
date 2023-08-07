9c89b226c5b246589bfd86f9ac50082be17602f6
Camila Paladines <camilapaladines27@gmail.com>
2023-08-07 15:34:38 -0500

    Navigation setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useRoutes, BrowserRouter} from 'react-router-dom';
+import './App.css';
+
+// Pages
+import Home from './pages/Home/Home';
+import MyAccount from './pages/MyAccount/MyAccount';
+import MyOrder from './pages/MyOrder/MyOrder';
+import MyOrders from './pages/MyOrders/MyOrders';
+import Signin from './pages/Signin/Signin';
+import NotFound from './pages/NotFound/NotFound';
+import NavBar from './components/NavBar/NavBar';
+
+const AppRoutes = () => {
+	return useRoutes([
+		{path: '/', element: <Home />},
+		{path: '/my-account', element: <MyAccount />},
+		{path: '/my-order', element: <MyOrder />},
+		{path: '/my-orders', element: <MyOrders />},
+		{path: '/signin', element: <Signin />},
+		{path: '*', element: <NotFound />},
+	]);
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<BrowserRouter>
+			<NavBar />
+			<AppRoutes />
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+export default function NavBar() {
+	const activeStyle = 'underline underline-offset-4';
+
+	return (
+		<nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light'>
+			<ul className='flex items-center gap-3'>
+				<li className='font-semibold text-lg'>
+					<NavLink to='/'>Shopi</NavLink>
+				</li>
+				<li>
+					<NavLink to='/' className={({isActive}) => (isActive ? activeStyle : '')}>
+						All
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/clothes' className={({isActive}) => (isActive ? activeStyle : '')}>
+						Clothes
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/electronics' className={({isActive}) => (isActive ? activeStyle : '')}>
+						Electronics
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/furnitures' className={({isActive}) => (isActive ? activeStyle : '')}>
+						Furnitures
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/toys' className={({isActive}) => (isActive ? activeStyle : '')}>
+						Toys
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/others' className={({isActive}) => (isActive ? activeStyle : '')}>
+						Others
+					</NavLink>
+				</li>
+			</ul>
+			<ul className='flex items-center gap-3'>
+				<li className='text-black/60'>camilapaladines27@gmail.com</li>
+				<li>
+					<NavLink to='my-orders' className={({isActive}) => (isActive ? activeStyle : '')}>
+						My orders
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='my-account' className={({isActive}) => (isActive ? activeStyle : '')}>
+						My account
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='signin' className={({isActive}) => (isActive ? activeStyle : '')}>
+						Sign in
+					</NavLink>
+				</li>
+				<li>Buy 0</li>
+			</ul>
+		</nav>
+	);
+}
