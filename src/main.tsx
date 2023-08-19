0bfe231d7a9351b5731b69abd37db19871499e6b
Camila Paladines <camilapaladines27@gmail.com>
2023-08-18 22:27:28 -0500

    Another change to deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
-import {ShopiProvider} from './context/index.tsx';
+import {ShopiProvider} from './context';
 
 import App from './App.tsx';
 
